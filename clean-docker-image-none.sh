1d76dd229ee0ab2784e90f6641bf8d2702bd7f92
Walter Wang <csw.dawm@gmail.com>
2017-05-22 19:04:35 +0800

    remove sudo

--- clean-docker-image-none.sh
+++ clean-docker-image-none.sh
@@ -6,6 +6,6 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-sudo docker rmi $(sudo docker images | grep '^<none>' | awk '{print $3}')
+docker rmi $(docker images | grep '^<none>' | awk '{print $3}')
 
 exit 0
